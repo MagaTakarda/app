72a4d7073cc7696f0bf56c1cd600be6c9bd06644
MagaTakarda <dahiruyahayayusuf0@gmail.com>
2023-03-21 22:23:58 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
 import { Routes, Route } from "react-router-dom"
-import home from  "./Home"
 import Landing from "./screens/Landing"
+import Home from "./screens/Home"
+import Login from "./screens/Login"
  
 function App() {
   return (
    <div className="app">
     <Routes>
       <Route path='landing' element={<Landing />} />
+      <Route path='/home' element={<Home />} />
+      <Route path='/login' element={<Login />} />
     </Routes>
 </div>
   );
